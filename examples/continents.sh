1cfc0f4ce01dcb9d771d62e17b74768448c1b2de
Hailey Weinschenk <106396387+reetyf@users.noreply.github.com>
2022-08-29 23:13:21 -0400

    scrape over continents

---
+++ examples/continents.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+arr=(Africa Antarctica Asia Europe North America South America Oceania)
+
+for continent in ${arr[@]}:
+do
+    python get_chart.py -p 10 -c "$continent" -y "1950-2022"
+    echo "Scraping $continent"
+done
