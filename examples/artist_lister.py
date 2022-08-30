82cbe294263c2479674b808ad65f18302ae24d9f
Hailey Weinschenk <106396387+reetyf@users.noreply.github.com>
2022-08-30 16:07:10 -0400

    took out dupes

--- examples/artist_lister.py
+++ examples/artist_lister.py
@@ -6,9 +6,19 @@ img_path = 'Exports/*'
 glob1 = glob.glob(img_path)
 glob2 = np.array(glob1)
 df = pd.DataFrame()
+
 for one_file in glob2:
     print(one_file)
     df_chunk = pd.read_csv(one_file, sep='\t', engine='python')
     df = pd.concat([df, df_chunk], axis=0)
+artists = df['Artist'].tolist()
+#only first artist in "artist 1 / artist 2"
+for art in artists:
+    if('/' in art):
+        artists.remove(art)
 
-np.savetxt('artists.txt', df['Artist'],fmt='%s',delimiter=',')
+#remove dupes
+artists= list(set(artists))
+
+#to txt file
+np.savetxt('artists.txt', artists ,fmt = '%s',delimiter='/',newline='\n')
