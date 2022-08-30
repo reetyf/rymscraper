a6385710ce521a69aebc99641292d18945af33e1
Hailey Weinschenk <106396387+reetyf@users.noreply.github.com>
2022-08-29 23:19:18 -0400

    Delete continents.sh

--- examples/continents.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-arr=(Africa Antarctica Asia Europe NorthAmerica SouthAmerica Oceania)
-
-for continent in ${arr[@]}:
-do
-    python get_chart.py -p 10 -c "$continent" -y "1950-2022"
-    echo "Scraping $continent"
-done
